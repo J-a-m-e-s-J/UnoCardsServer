75d68151715c5cae204e8780a23bfb5fa53b7a71
1 <jamesjfy@163.com>
2025-01-12 19:47:43 +0800

    add a db

--- UnoCardsServer/Program.cs
+++ UnoCardsServer/Program.cs
@@ -1,83 +1,84 @@
-﻿using System.Net;
+﻿namespace UnoCardsServer;
+
+using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Data.SQLite;
 
-namespace UnoCardsServer
+public static class UnoCardsServer
 {
-    public static class UnoCardsServer
+    private static Socket _server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+    private static byte[] _buffer = new byte[1024];
+    private static string? _func;
+    private static List<Socket> _userList = new List<Socket>();
+    
+    public static void Main(string[] args)
     {
-        private static Socket _socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-        private static byte[] _buffer = new byte[1024];
-        private static string? _func;
-        private static List<Socket> _userList = new List<Socket>();
-        
-        public static void Main(string[] args)
+        // _socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        _server.Bind(new IPEndPoint(IPAddress.Any, 25565));
+        _server.Listen();
+        StartAccept();
+        while (true)
         {
-            // _socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-            _socket.Bind(new IPEndPoint(IPAddress.Any, 25565));
-            _socket.Listen();
-            StartAccept();
-            while (true)
-            {
-                _func = Console.ReadLine();
+            _func = Console.ReadLine();
 
-                switch (_func)
-                {
-                    case "exit":
-                        break;
-                    
-                    case "":
-                        continue;
-                    
-                    default:
-                        Console.WriteLine("Invalid function");
-                        continue;
-                }
+            switch (_func)
+            {
+                case "exit":
+                    Console.WriteLine("服务端已退出");
+                    break;
+                
+                case "":
+                    continue;
+                
+                default:
+                    Console.WriteLine("Invalid function");
+                    continue;
             }
         }
+    }
 
-        static void SendMsg(Socket client, byte[] buffer)
-        {
-            _socket.Send(buffer);
-        }
+    static void SendMsg(Socket client, byte[] buffer)
+    {
+        client.Send(buffer);
+    }
 
-        static void SendMsg(Socket client)
-        {
-            _socket.Send(_buffer);
-        }
+    static void SendMsg(Socket client)
+    {
+        client.Send(_buffer);
+    }
 
-        static void StartAccept()
-        {
-            _socket.BeginAccept(AcceptCallback, null);
-        }
+    static void StartAccept()
+    {
+        _server.BeginAccept(AcceptCallback, null);
+    }
 
-        static void AcceptCallback(IAsyncResult iar)
-        {
-            Socket client = _socket.EndAccept(iar);
-            _userList.Add(client);
-            StartRecieve(client);
-            client.Close();
-            StartAccept();
-        }
+    static void AcceptCallback(IAsyncResult iar)
+    {
+        Socket client = _server.EndAccept(iar);
+        _userList.Add(client);
+        StartRecieve(client);
+        client.Close();
+        StartAccept();
+    }
 
-        static void StartRecieve(Socket client)
-        {
-            client.BeginReceive(_buffer, 0, _buffer.Length, SocketFlags.None, RecieveCallback, client);
-        }
+    static void StartRecieve(Socket client)
+    {
+        client.BeginReceive(_buffer, 0, _buffer.Length, SocketFlags.None, RecieveCallback, client);
+    }
 
-        static void RecieveCallback(IAsyncResult iar)
+    static void RecieveCallback(IAsyncResult iar)
+    {
+        Socket client = (iar.AsyncState as Socket)!;
+        int len = client.EndReceive(iar);
+        if (len == 0)
         {
-            Socket client = (iar.AsyncState as Socket)!;
-            int len = client.EndReceive(iar);
-            if (len == 0)
-            {
-                _userList.Remove(client);
-                return;
-            }
-            string message = Encoding.UTF8.GetString(_buffer, 0, len);
-            Console.WriteLine(message);
-            client.Close();
-            StartRecieve(client);
+            _userList.Remove(client);
+            return;
         }
+        string message = Encoding.UTF8.GetString(_buffer, 0, len);
+        Console.WriteLine(message);
+        client.Close();
+        StartRecieve(client);
     }
 }
--- UnoCardsServer/obj/Debug/net8.0/UnoCardsServer.AssemblyInfo.cs
+++ UnoCardsServer/obj/Debug/net8.0/UnoCardsServer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UnoCardsServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7fa62378b8084254a654fe8199b36f725ade1e1e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+357fc18a2035d74f4251a961f80dcffed7fdf047")]
 [assembly: System.Reflection.AssemblyProductAttribute("UnoCardsServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UnoCardsServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
