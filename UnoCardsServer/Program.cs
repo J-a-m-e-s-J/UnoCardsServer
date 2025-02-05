2d3464fb17572d9298421d1a6fc57f5651e55c14
1 <jamesjfy@163.com>
2025-02-05 13:48:22 +0800

    sth new

--- UnoCardsServer/Program.cs
+++ UnoCardsServer/Program.cs
@@ -9,8 +9,14 @@ public static class UnoCardsServer
 {
     private static Socket _server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
     private static byte[] _buffer = new byte[1024];
-    private static string? _func;
+    private static string _func = null!;
+    private static string _funcInput = null!;
+    private static string _parameter = null!;
+    private static List<string> _parameters = new List<string>();
     private static List<Socket> _userList = new List<Socket>();
+    private static List<string> _funcs = new List<string>() { "exit", "log", "" };
+    private static bool _isRunning = true;
+    private static object _lock = new object();
     
     public static void Main(string[] args)
     {
@@ -18,24 +24,10 @@ public static class UnoCardsServer
         _server.Bind(new IPEndPoint(IPAddress.Any, 25565));
         _server.Listen();
         StartAccept();
-        while (true)
-        {
-            _func = Console.ReadLine();
-
-            switch (_func)
-            {
-                case "exit":
-                    Console.WriteLine("服务端已退出");
-                    break;
-                
-                case "":
-                    continue;
-                
-                default:
-                    Console.WriteLine("Invalid function");
-                    continue;
-            }
-        }
+        Thread handleFunc = new Thread(HandleFuncs);
+        Thread handleFuncInput = new Thread(HandleInputFuncs);
+        handleFunc.Start();
+        handleFuncInput.Start();
     }
 
     static void SendMsg(Socket client, byte[] buffer)
@@ -58,7 +50,6 @@ public static class UnoCardsServer
         Socket client = _server.EndAccept(iar);
         _userList.Add(client);
         StartRecieve(client);
-        client.Close();
         StartAccept();
     }
 
@@ -69,7 +60,7 @@ public static class UnoCardsServer
 
     static void RecieveCallback(IAsyncResult iar)
     {
-        Socket client = (iar.AsyncState as Socket)!;
+        Socket client = (Socket)iar.AsyncState!;
         int len = client.EndReceive(iar);
         if (len == 0)
         {
@@ -77,8 +68,109 @@ public static class UnoCardsServer
             return;
         }
         string message = Encoding.UTF8.GetString(_buffer, 0, len);
-        Console.WriteLine(message);
-        client.Close();
+        // Console.WriteLine(message);
+        HandleMessage(message);
+        // client.Close();
         StartRecieve(client);
+    }
+    
+    static void HandleMessage(string message)
+    {
+        lock (_lock)
+        {
+            char[] separators = new[] { ' ' };
+            List<string> parts = message.Split(separators).ToList();
+            _func = parts[0];
+
+            if (!_funcs.Contains(_func))
+            {
+                Console.WriteLine("Invalid function");
+                return;
+            }
+
+            if (_func == "")
+            {
+                return;
+            }
+            
+            _parameter = parts[1];
+            _parameters = _parameter.Split('(', ')', ' ').ToList();
+        }
+        Console.WriteLine(message);
+        // Console.WriteLine("msg:" + message);
+    }
+    
+    static void HandleMessage(string message, char separator)
+    {
+        
+    }
+
+    static void HandleMessage(string message, char[] separators)
+    {
+        
+    }
+
+    static void HandleFuncs()
+    {
+        while (_isRunning)
+        {
+            lock (_lock)
+            {
+                SwitchFunc(_func, _parameters);
+            }
+        }
+    }
+
+    static void HandleInputFuncs()
+    {
+        while (_isRunning)
+        {
+            lock (_lock)
+            {
+                _funcInput = Console.ReadLine()!;
+
+                try
+                {
+                    HandleMessage(_funcInput);
+                    SwitchFunc(_funcInput, _parameters);
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Wrong input!\nInput parameters!");
+                    _func = "";
+                    // Console.WriteLine(e);
+                    continue;
+                }
+            }
+        }
+    }
+
+    static void SwitchFunc(string func, List<string> parameters)
+    {
+        lock (_lock)
+        {
+            switch (func)
+            {
+                case "exit":
+                    Console.WriteLine("服务端已退出");
+                    _isRunning = false;
+                    break;
+
+                case "log":
+                    Console.WriteLine(func);
+                    var messageLog = parameters[0];
+                    if (messageLog.Contains(":ip"))
+                    {
+                        foreach (var i in messageLog)
+                        {
+                            
+                        }
+                    }
+                    break;
+                
+                case "":
+                    break;
+            }
+        }
     }
 }
